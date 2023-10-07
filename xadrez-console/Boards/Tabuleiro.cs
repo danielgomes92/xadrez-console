96d8f34d590d34d95898533fab3d2f7ccce3bfe5
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2023-10-07 08:34:53 -0300

    Criando e testando erros  com excecao personalizadas

--- xadrez-console/Boards/Tabuleiro.cs
+++ xadrez-console/Boards/Tabuleiro.cs
@@ -18,10 +18,38 @@
             return Pecas[linha, coluna];
         }
 
+        public Peca peca(Posicao pos)
+        {
+            return Pecas[pos.Linha, pos.Coluna];
+        }
+
+        public bool SeExistePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+
         public void ColocarPeca(Peca p, Posicao pos)
         {
+            if (SeExistePeca(pos))
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+
             Pecas[pos.Linha, pos.Coluna] = p;
             p.PosicaoPeca = pos;
+        }
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if(pos.Linha < 0 || pos.Linha >= Linhas || pos.Coluna < 0 || pos.Coluna >= Colunas)
+                return false;
+
+            return true;
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+                throw new TabuleiroException("Posição inválida!");
         }
     }
 }
---
+++ xadrez-console/Boards/TabuleiroException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xadrez_console.Boards
+{
+    internal class TabuleiroException : Exception
+    {
+        public TabuleiroException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
